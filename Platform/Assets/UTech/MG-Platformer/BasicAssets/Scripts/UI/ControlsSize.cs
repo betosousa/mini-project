252d7a0dc04d63cf1e2e159546b745fd23c85806
Alberto Sousa <avss@cin.ufpe.br>
2019-09-23 20:02:11 -0300

    filling up the menu

---
+++ Platform/Assets/UTech/MG-Platformer/BasicAssets/Scripts/UI/ControlsSize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Platformer.UI
+{
+    public class ControlsSize : MonoBehaviour
+    {
+        public Slider slider;
+        public RectTransform axis, jumpButton;
+
+        public void UpdateSize()
+        {
+            float size = slider.value;
+            
+            axis.localScale = new Vector3(size, size, 1f);
+            jumpButton.localScale = new Vector3 (size, size, 1f);
+        }
+
+        public void Default()
+        {
+            slider.value = 1f;
+            UpdateSize();
+        }
+    }
+}
